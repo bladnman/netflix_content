6d78e036287dab9f567348403c06d978834f74bf
Matt Maher <matt@maher-family.net>
2022-05-22 12:43:01 -0700

    genre looking

--- utils/putils/reader.py
+++ utils/putils/reader.py
@@ -1,1 +1,23 @@
-# placeholder
+import pandas as pd
+from mbmutils import mu
+from ast import literal_eval
+
+
+def read_titles() -> pd.DataFrame:
+    df = pd.read_csv(mu.get_full_path("data/titles.csv"))
+
+    # clean genres to be real lists
+    df['genres'] = df['genres'].apply(literal_eval)
+
+    # get year values into date object
+    pd.to_datetime(df.release_year, format='%Y')
+
+    # create decade values
+    df["decade"] = (df.release_year//10)*10
+
+    return df
+
+
+def read_people() -> pd.DataFrame:
+    df = pd.read_csv(mu.get_full_path("data/credits.csv"))
+    return df
